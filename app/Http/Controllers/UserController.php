4f70bdd1ac3c65fce198d9aed125851848555d6b
JaspherXIII <157330223+JaspherXIII@users.noreply.github.com>
2025-01-24 00:56:52 +0800

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -43,7 +43,7 @@ class UserController extends Controller
             [
                 'name' => $request->name,
                 'email' => $request->email,
-                'password' => bcrypt($request->password), 
+                'password' => Hash::make($request->password),
             ]
         );
 
