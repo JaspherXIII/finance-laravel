1668e490e5e257ec5eeca8c77b2d124b85239119
JaspherXIII <157330223+JaspherXIII@users.noreply.github.com>
2025-02-23 21:24:31 +0800

    Update index.blade.php

--- resources/views/dashboards/admins/index.blade.php
+++ resources/views/dashboards/admins/index.blade.php
@@ -20,17 +20,11 @@
                         <div class="card card-block card-stretch card-height">
                             <div class="card-body">
                                 <div class="d-flex align-items-center mb-4 card-total-sale">
-                                    <div class="icon iq-icon-box-2 bg-info-light">
-                                        <img src="../assets/images/product/1.png" class="img-fluid" alt="image">
-                                    </div>
                                     <div>
                                         <p class="mb-2">Total Purchase Order</p>
                                         <h4 id="totalOrders">Loading...</h4>
                                     </div>
                                 </div>
-                                <div class="iq-progress-bar mt-2">
-                                    <span class="bg-info iq-progress progress-1" data-percent="85"></span>
-                                </div>
                             </div>
                         </div>
                     </div>
@@ -38,17 +32,11 @@
                         <div class="card card-block card-stretch card-height">
                             <div class="card-body">
                                 <div class="d-flex align-items-center mb-4 card-total-sale">
-                                    <div class="icon iq-icon-box-2 bg-danger-light">
-                                        <img src="../assets/images/product/2.png" class="img-fluid" alt="image">
-                                    </div>
                                     <div>
                                         <p class="mb-2">Total Products Ordered</p>
                                         <h4 id="totalProducts">Loading...</h4>
                                     </div>
                                 </div>
-                                <div class="iq-progress-bar mt-2">
-                                    <span class="bg-danger iq-progress progress-1" data-percent="70"></span>
-                                </div>
                             </div>
                         </div>
                     </div>
@@ -56,16 +44,11 @@
                         <div class="card card-block card-stretch card-height">
                             <div class="card-body">
                                 <div class="d-flex align-items-center mb-4 card-total-sale">
-                                    <div class="icon iq-icon-box-2 bg-success-light">
-                                        <img src="../assets/images/product/3.png" class="img-fluid" alt="image">
-                                    </div>
+                                    
                                     <div>
                                         <p class="mb-2">Total Amount</p>
                                         <h4 id="totalAmount">Loading...</h4>
                                     </div>
-                                </div>
-                                <div class="iq-progress-bar mt-2">
-                                    <span class="bg-success iq-progress progress-1" data-percent="75"></span>
                                 </div>
                             </div>
                         </div>
